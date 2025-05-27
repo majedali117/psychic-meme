3110f66704994e7d044db2e2aa1f53a634515931
Majed <Majed.ali@hqplus.de>
2025-05-27 23:11:20 +0200

    vite updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base : "/psychic-meme/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
